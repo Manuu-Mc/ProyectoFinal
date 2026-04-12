1e36b4cd2e068e7dfa76535daaf73dc2c5a2fcc8
Manuel <manuela.montenegroc@uqvirtual.edu.co>
2026-04-12 18:01:02 -0500

    Inicial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module uniquindio.edu.co.proyectofinalcodigo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens uniquindio.edu.co.proyectofinalcodigo to javafx.fxml;
+    exports uniquindio.edu.co.proyectofinalcodigo;
+}
